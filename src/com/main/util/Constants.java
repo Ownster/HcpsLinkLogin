786028b94a219de84a6b67016f8942b1a85754af
Ownster <coderjacob2@gmail.com>
2014-06-08 17:32:32 -0400

    Update

---
+++ src/com/main/util/Constants.java
@@ -0,0 +1,7 @@
+package com.main.util;
+
+public class Constants {
+
+	public static String user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36";
+	
+}
